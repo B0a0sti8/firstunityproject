0acd916d85201aad755babf8bd65b9b31fce292b
Marcel <marsipulami0107@gmail.com>
2021-11-11 23:36:50 +0100

    Movement, Camera and Collision Test

---
+++ Assets/Skripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float _Speed;
+
+    Vector2 _Movement; // declare variable
+
+    Rigidbody2D _Rigidbody; // get access to rigidbody
+
+    private void Awake()
+    {
+        _Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnMovement(InputValue value) // OnMovement = On + name of action-input 
+    {
+        _Movement = value.Get<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        _Rigidbody.velocity = _Movement * _Speed;
+    }
+}
