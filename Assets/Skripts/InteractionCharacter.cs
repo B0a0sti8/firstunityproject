7511fa6503f5a1374fa38d305e4160bda47e0f62
Sebastian <sebimueller.olching@googlemail.com>
2021-11-13 17:09:50 +0100

    Interaktion Fortsetzung, Beginn Items

--- Assets/Skripts/InteractionCharacter.cs
+++ Assets/Skripts/InteractionCharacter.cs
@@ -1,3 +1,8 @@
+// Dieses Skript sorgt dafür dass bei Rechtsklick überprüft wird, ob auf ein Interaktives Objekt geklickt wurde
+// z.B. Items, Truhen, Händler etc.
+// Eng verbunden mit "Interactable"-Skript
+// Verantwortlicher für den Quatsch: Basti
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -25,24 +30,38 @@ public class InteractionCharacter : MonoBehaviour // Sorry Marcus, ist echt nich
         {
             RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()), Vector2.zero); // Die Mausposition (ursprünglich in Pixel) wird in Weltkoordinaten übersetzt (Unity transform z.B.)
                                                                                                                                     // Eine Linie zwischen der Position des Mauszeigers wird gebildet und dem Vektor (0,0) wird gebildet. Ist noch nicht ganz klar ob das der Ursprung der Map oder das Zentrum der Kamera ist.
+            // Debug.DrawRay(Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()), new Vector3(10f, 10f, 0f), Color.blue, 20f); Anzeige für Referenz-Ray
+
             if (hit.collider != null)   // Wird geprüft ob überhaupt was getroffen wurde
             {
-                if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Interactable"))        // Überprüfung ob der Gegenstand in der Layer "Interactible" ist
-                {
-                    Interactable interactable = hit.collider.GetComponent<Interactable>();          // Gegenstand der getroffen wurde wird fokusiert. (Für spätere Interaktion)
-                    SetFocus(interactable);
-                }
+                 Interactable interactable = hit.collider.GetComponent<Interactable>();          // Gegenstand der getroffen wurde wird fokusiert. (Für spätere Interaktion)
+                 SetFocus(interactable);
             }
         }
     }
 
-    void SetFocus (Interactable newFocus)
+    void SetFocus (Interactable newFocus) 
     {
-        focus = newFocus;
+        if (newFocus != focus)
+        {
+            if (focus != null)
+            {
+                focus.OnDefocused(); // Legt Fokus des Charakters auf getroffenen interaktiven Gegenstand
+            }
+
+            focus = newFocus;
+        }
+
+        newFocus.OnFocused(transform);
     }
 
-    void RemoveFocus ()
+    void RemoveFocus ()             // Entfernt Fokus des Charakters
     {
+        if (focus != null)
+        {
+            focus.OnDefocused();
+        }
+
         focus = null;
     }
 }
