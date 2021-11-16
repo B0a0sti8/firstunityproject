cfd05197c0a02f25762601cb84226632171bb97a
Marcel <marsipulami0107@gmail.com>
2021-11-16 19:33:07 +0100

    Epic menus with epic sound

---
+++ Assets/Skripts/MainMenue.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// inspired by: Brackeys - START MENU in Unity
+
+public class MainMenue : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        //FindObjectOfType<AudioManager>().Play("Click"); // click-sound when pressing the button (optional)
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // load next level in the queue
+        // or load special scene with the name: SceneManager.LoadScene("Level_XY");
+        // or with the build-index: SceneManager.LoadScene(1);
+
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
