d1629f80d84fa447bcc2d7a91ec633c425226efa
Sebastian <sebimueller.olching@googlemail.com>
2021-12-20 00:00:17 +0100

    Multiplayer Enemies

--- Assets/Skripts/Enemy/MeleeEnemyAttackTest.cs
+++ Assets/Skripts/Enemy/MeleeEnemyAttackTest.cs
@@ -7,8 +7,6 @@ public class MeleeEnemyAttackTest : EnemyAttack
     public override void EnemyAtkEffect(GameObject target)
     {
         base.EnemyAtkEffect(target);
-        Debug.Log("OH YEEAH! :3 " + target);
-        //target.GetComponent<Player>().TakeDamage(20);
         target.GetComponent<PlayerStats>().TakeDamage(gameObject.GetComponent<EnemyStats>().damage.GetValue());
         // Attack
         // Animation
