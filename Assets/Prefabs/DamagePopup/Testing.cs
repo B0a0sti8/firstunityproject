e5e5be285d042108b7947695d0bb4c4f72b13ff1
Sebastian <sebimueller.olching@googlemail.com>
2022-01-02 23:21:45 +0100

    Damage Popup

---
+++ Assets/Prefabs/DamagePopup/Testing.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    private void Start()
+    {
+        DamagePopup.Create(Vector3.zero, 300, false);
+    }
+
+    private void Update()
+    {
+        //DamagePopup.Create(Vector3.zero, 300, false);
+    }
+}
