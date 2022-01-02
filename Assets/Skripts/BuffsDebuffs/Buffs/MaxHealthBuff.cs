4f2b0d252f96b0218c99b8754dd0ab2fa97659af
Marcel <marsipulami0107@gmail.com>
2022-01-02 23:20:53 +0100

    Minor Changes

--- Assets/Skripts/BuffsDebuffs/Buffs/MaxHealthBuff.cs
+++ Assets/Skripts/BuffsDebuffs/Buffs/MaxHealthBuff.cs
@@ -2,23 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpeedBoostBuff : Buff
+public class MaxHealthBuff : Buff
 {
+    public override void Dispell()
+    {
+        isRemovable = false;
+        base.Dispell();
+    }
+
     public override void StartBuffEffect(PlayerStats playerStats)
     {
         base.StartBuffEffect(playerStats);
-        playerStats.movementSpeed.AddModifierAdd(value);
+        playerStats.maxHealth.AddModifierAdd(value);
+        playerStats.currentHealth += value;
     }
 
     public override void EndBuffEffect(PlayerStats playerStats)
     {
         base.EndBuffEffect(playerStats);
-        playerStats.movementSpeed.AddModifierAdd(-value);
+        playerStats.maxHealth.AddModifierAdd(-value);
     }
 
     public override Buff Clone()
     {
-        SpeedBoostBuff clone = (SpeedBoostBuff)this.MemberwiseClone();
+        MaxHealthBuff clone = (MaxHealthBuff)this.MemberwiseClone();
         return clone;
     }
 }
